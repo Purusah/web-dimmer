02abba9ea87007e7205114f5a53d57feed6d08ad
purusah <bohaichuk.v@gmail.com>
2022-09-18 00:36:46 +0300

    Update user-agent

--- src/background/index.js
+++ src/background/index.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const userAgentHeader = "user-agent";
-const userAgentValue = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-    + "(KHTML, like Gecko) Chrome/99.0.4844 Safari/537.36";
+const userAgentValue = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+    "(KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36";
 
 /**
  * @param {browser.webRequest._OnBeforeSendHeadersDetails} details
